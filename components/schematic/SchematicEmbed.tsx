e96aa9f04a711d39c81a3e70a27c98e949fb4331
unknown <arhanansari2009@gmail.com>
2025-03-01 13:36:23 +0530

    Some changes

--- components/schematic/SchematicEmbed.tsx
+++ components/schematic/SchematicEmbed.tsx
@@ -1,15 +1,6 @@
 "use client";
 import { SchematicEmbed as SchematicEmbedComponent } from "@schematichq/schematic-components";
 
-// const SchematicEmbed = () => {
-//   const token = " ... " 
-//   const componentId = "cmpn_RRPvTY9FaA6"
-
-//   return (
-//     <SchematicEmbedComponent accessToken={token} id={componentId} />
-//   )
-// }
-
 const SchematicEmbed = ({
   accessToken,
   componentId,
