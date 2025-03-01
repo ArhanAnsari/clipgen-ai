723c7bd5437c2447dbce73b6e832df88e918f2f7
Arhan Ansari <arhanansari2009@gmail.com>
2025-03-01 09:02:26 +0530

    Update AiAgentChat.tsx

--- components/AiAgentChat.tsx
+++ components/AiAgentChat.tsx
@@ -1,8 +1,14 @@
 "use client";
 
 import { useChat } from "@ai-sdk/react";
+import { createGoogleGenerativeAI } from "@ai-sdk/google";
+
+const google = createGoogleGenerativeAI({
+apiKey: process.env.GEMINI_API_KEY,
+});
 
 function AiAgentChat(){
+  const model = google("gemini-2.0-flash");
   return <div>AiAgentChat</div>
 }
 
